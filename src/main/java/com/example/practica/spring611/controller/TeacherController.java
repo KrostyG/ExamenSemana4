771db00f0971dc099d35fc8d80790bb78157fb89
edugl9 <93335957+edugl9@users.noreply.github.com>
2021-11-18 17:40:11 -0600

    Add files via upload

---
+++ src/main/java/com/example/practica/spring611/controller/TeacherController.java
@@ -0,0 +1,49 @@
+package com.example.practica.spring611.controller;
+
+import com.example.practica.spring611.entity.Teacher;
+import com.example.practica.spring611.entity.Teacher2;
+import com.example.practica.spring611.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TeacherController {
+
+    @Autowired
+    private TeacherService serviceteacher;
+
+
+    //Creacion del maestro
+    @PostMapping("/agregarteacher")
+    public ResponseEntity<Teacher> addTeacher(@RequestBody Teacher teacher) {
+        System.out.println("Se hizo la solicitud");
+        serviceteacher.AddTeacher(teacher);
+        return ResponseEntity.ok(teacher);
+    }
+
+
+    @GetMapping("/teacher/{pornombre}/{nombrevariable}")
+    public Teacher getTeacherByName(@PathVariable("nombrevariable") String nombre) {
+        return serviceteacher.getTeacher(nombre);
+    }
+
+    @GetMapping("/teacher2/{edad}")
+    public List<Teacher2> getTeacherByName2(@PathVariable("edad") Integer edad) {
+        return serviceteacher.getTeacher2(edad);
+    }
+
+    @GetMapping("/teacher3/{edad}")
+    public List<String> getTeacherByName3(@PathVariable("edad") Integer edad) {
+        return serviceteacher.getTeacher3(edad);
+    }
+
+    @GetMapping("/teacherPorNombreCurso/{nombre}")
+    public List<Teacher> getTeacherByNameJoinCurso(@PathVariable("nombre") String nombre) {
+        return serviceteacher.getTeacherByNameJoinCurso(nombre);
+    }
+}
+
---
+++ src/main/java/com/example/practica/spring611/service/TeacherService.java
@@ -0,0 +1,57 @@
+package com.example.practica.spring611.service;
+
+import com.example.practica.spring611.entity.Student;
+import com.example.practica.spring611.entity.Teacher;
+import com.example.practica.spring611.entity.Teacher2;
+import com.example.practica.spring611.repository.StudentRepository;
+import com.example.practica.spring611.repository.TeacherRepository;
+import com.example.practica.spring611.repository.TeacherRepositoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeacherService {
+
+    @Autowired
+    TeacherRepositoryDao teacherRepositoryDao;
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    public void AddTeacher(Teacher teacher) {
+
+        teacherRepository.creacionMaestros(teacher);
+
+    }
+
+    public List<Teacher> getLista() {
+        return null;
+    }
+
+    public Teacher getTeacher(String name) {
+        return null;
+    }
+
+    public Teacher2 getTeacher2(String nombre) {
+        return null;
+    }
+
+    public List<Teacher2> getTeacher2(Integer id) {
+        return teacherRepositoryDao.findTeacherByNombreAndAndEdadPorJPQueryN3(id);
+    }
+
+    public List<String> getTeacher3(Integer id) {
+        return teacherRepositoryDao.findTeacherByNombreAndAndEdadPorJPQueryN2(id);
+    }
+
+    public List<Teacher> getTeacherByNameJoinCurso(String nombre) {
+        return teacherRepositoryDao.findTeacherByNombreJoinCurso(nombre);
+    }
+
+
+    //public List<Teacher> getTeacherByNameAndAge(String name, int edad) {
+    //  return teacherRepositoryDao.findTeacherByNombreAndAndEdad(name, edad);
+    //}
+}
